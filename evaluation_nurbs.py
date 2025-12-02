a31996b4c5947aa265e00df3c599d23eb6a7d2c5
Yaxi <164341125+YaxiiC@users.noreply.github.com>
2025-12-03 06:25:45 +0800

    Update dilation iterations in mask generation
    Increased dilation iterations from 2 to 4 for mask generation.

--- evaluation_nurbs.py
+++ evaluation_nurbs.py
@@ -199,7 +199,7 @@ def _evaluate_single(
 
     fill_solid = roi_name in ("femur", "tibia")
     pred_mask = _points_to_mask(
-        pred_points_np, volume_crop.shape, spacing, dilation_iters=2, fill_solid=fill_solid
+        pred_points_np, volume_crop.shape, spacing, dilation_iters=4, fill_solid=fill_solid
     )
 
     dsc = dice_score(pred_mask, mask_crop)
